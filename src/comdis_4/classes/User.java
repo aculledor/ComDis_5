c641e4ca87b5882999b62eec130edb1f6d593ad5
Abraham Trashorras <abraham.cocodin@gmail.com>
2021-03-31 18:56:09 +0200

    User is defined

--- src/comdis_4/classes/User.java
+++ src/comdis_4/classes/User.java
@@ -5,10 +5,10 @@
  */
 package comdis_4.classes;
 
-import comdis_4.client.ClientImplementation;
 import comdis_4.client.ClientInterface;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  *
@@ -17,33 +17,19 @@ import java.util.ArrayList;
 public class User implements Serializable{
     private String nickname;
     private String password;
-    private ArrayList<String> friends;
-    private ClientInterface myProxy;
-    private ArrayList<ClientInterface> friendsProxys;
+    private ClientInterface proxy;
+    private HashMap<String, ClientInterface> friendsProxys;
+    private ArrayList<String> friendRequests;
 
     public User() {
     }
 
-    public User(String nickname, String password, ArrayList<String> friends) {
+    public User(String nickname, String password) {
         this.nickname = nickname;
         this.password = password;
-        this.friends = friends;
+        this.proxy = null;
         this.friendsProxys = null;
-    }
-
-    public User(String nickname, String password, ArrayList<String> friends, ArrayList<ClientInterface> friendsProxys) {
-        this.nickname = nickname;
-        this.password = password;
-        this.friends = friends;
-        this.friendsProxys = friendsProxys;
-    }
-
-    public User(String nickname, String password, ArrayList<String> friends, ClientInterface myProxy, ArrayList<ClientInterface> friendsProxys) {
-        this.nickname = nickname;
-        this.password = password;
-        this.friends = friends;
-        this.myProxy = myProxy;
-        this.friendsProxys = friendsProxys;
+        this.friendRequests = null;
     }
 
     public String getNickname() {
@@ -64,46 +50,30 @@ public class User implements Serializable{
         return this;
     }
 
-    public ArrayList<String> getFriends() {
-        return friends;
+    public HashMap<String, ClientInterface> getFriendsProxys() {
+        return friendsProxys;
     }
 
-    public User setFriends(ArrayList<String> friends) {
-        this.friends = friends;
-        return this;
-    }
-
-    public User addFriends(String friend) {
-        this.friends.add(friend);
-        return this;
-    }
-
-    public User removeFriends(String friend) {
-        this.friends.remove(friend);
-        return this;
-    }
-
-    public User setFriendsProxys(ArrayList<ClientInterface> friendsProxys) {
+    public User setFriendsProxys(HashMap<String, ClientInterface> friendsProxys) {
         this.friendsProxys = friendsProxys;
         return this;
     }
 
-    public User addFriendsProxys(ClientImplementation friendProxys) {
-        this.friendsProxys.add(friendProxys);
-        return this;
-    }
-
-    public User removeFriendsProxys(ClientImplementation friendProxys) {
-        this.friendsProxys.remove(friendProxys);
-        return this;
-    }
-
-    public ClientInterface getMyProxy() {
-        return myProxy;
+    public ClientInterface getProxy() {
+        return proxy;
     }
 
     public User setMyProxy(ClientInterface myProxy) {
-        this.myProxy = myProxy;
+        this.proxy = myProxy;
+        return this;
+    }
+
+    public ArrayList<String> getFriendRequests() {
+        return friendRequests;
+    }
+
+    public User setFriendRequests(ArrayList<String> friendRequests) {
+        this.friendRequests = friendRequests;
         return this;
     }
     
@@ -114,11 +84,17 @@ public class User implements Serializable{
         String text = "\nNickname: "+nickname
                 +". Password: "+password
                 +"\nFriends:\n";
-        if(friends != null && !friends.isEmpty()){
-            for(String friend : friends){
-                text = text + "\t-> "+friend+"\n";
-            }
-        }
+                if(friendsProxys != null && !friendsProxys.isEmpty()){
+                    for(String friend : friendsProxys.keySet()){
+                        text = text + "\t-> "+friend+"\n";
+                    }
+                }
+                text = text +"\nFriendship request:\n";
+                if(friendRequests != null && !friendRequests.isEmpty()){
+                    for(String friend : friendRequests){
+                        text = text + "\t-> "+friend+"\n";
+                    }
+                }
         text = text +"\n";
         return text; //To change body of generated methods, choose Tools | Templates.
     }
@@ -127,10 +103,5 @@ public class User implements Serializable{
     public int hashCode() {
         return super.hashCode(); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
-    
-    
-    
+ 
 }
